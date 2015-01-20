340cb798fe4791ceedb2cd8a0d3d2f4075c97d55
SimoPrG <SimoPrG@users.noreply.github.com>
2015-01-20 17:11:02 +0200

    Solution to Problem 10. Fibonacci Numbers

--- Console-IO-Homework/FibonacciNumbers/FibonacciNumbers.cs
+++ Console-IO-Homework/FibonacciNumbers/FibonacciNumbers.cs
@@ -18,6 +18,23 @@ class FibonacciNumbers
 {
     static void Main()
     {
+        Console.Title = "Fibonacci Numbers"; //Changing the title of the console.
 
+        Console.Write("Enter an integer n to see the first n members of the Fibonacci sequence: ");
+        int n = int.Parse(Console.ReadLine());
+        string[] fiboMembers = new string[n]; // This array will keep the members of the Fibonacci sequence.
+
+        for (long i = 0, previousMember = 0, nextMember = 1, temp; i < n; i++)
+        // In the for loop we declare the first two members of the sequence (0 and 1) and a temp variable 
+        // needed for the calculations.
+        {
+            fiboMembers[i] = previousMember.ToString(); //We convert the member to string and we place it in its array position.
+            temp = nextMember;//After that we assign the value of the next member to temp.
+            nextMember += previousMember;//Then we add the previous member to the next member.
+            previousMember = temp;//Finally, we assign the value of the temp to the previous member.
+        }
+        Console.WriteLine(string.Join(", ",fiboMembers)); //Creating a string in the requested format and printing it.
+
+        Console.ReadKey(); // Keeping the console opened.
     }
 }
