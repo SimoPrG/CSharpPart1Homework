96dfb0d1bcaa5bf6086ca3326eeca0f5ddc2cf17
SimoPrG <simeon_petev@hotmail.com>
2016-01-31 22:22:22 +0200

    Small test

--- Primitive-Data-Types-Homework/FloatOrDouble/FloatOrDouble.cs
+++ Primitive-Data-Types-Homework/FloatOrDouble/FloatOrDouble.cs
@@ -1,7 +1,7 @@
 ﻿/*Problem 2. Float or Double?
 
     Which of the following values can be assigned to a variable of type float and which to a variable
-    of type double: 34.567839023, 12.345, 8923.1234857, 3456.091?
+    of type double: 34.567839023, 12.34, 8923.1234857, 3456.091?
 
     Write a program to assign the numbers in variables and print them to ensure no precision is lost.
 */
@@ -16,7 +16,7 @@ class FloatOrDouble
         Console.Title = "Float or Double";
 
         double firstVariable = 34.567839023d;
-        float secondVariable = 12.345f;
+        float secondVariable = 12.34f;
         double thirdVariable = 8923.1234857d;
         float fourthVariable = 3456.091f;
 
